eb141bc0ec0efcd4e97968b37ea16dabe52f60e1
Mrt1ns <72765821+Mrt1ns@users.noreply.github.com>
2022-07-17 16:04:44 -0300

    commit via vscode

---
+++ Desafio04.py
@@ -0,0 +1,5 @@
+n = input (' Digite um algo: ')
+
+print ('O que foi digitado e numerico: {}'.format(n.isnumeric()))
+print ('O que foi digitado e alfabetico: {}'.format(n.isalpha()))
+print ('O que foi diggitado e alfanumerico {}'.format(n.isascii()))
