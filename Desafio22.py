d1b2fc0e57eefb661d4083b7a769c4c4399f156d
Mrt1ns <72765821+Mrt1ns@users.noreply.github.com>
2022-07-22 21:36:48 -0300

    Commit via vscode

--- Desafio22.py
+++ Desafio22.py
@@ -1,17 +1,5 @@
 nome = input(' Digite seu nome completo: ')
 
-#maiscula = print(nome.upper())
-
-#minuscula = print(nome.lower())\
-
-#junto = nome.replace(' ', '')
-
-#print ('Tem exatamente {} letras, no nome {}'.format(len(junto), nome))
-
-#div = nome.split()
-
-#print(div[0])
-
 print(nome.upper())
 
 print(nome.lower())
